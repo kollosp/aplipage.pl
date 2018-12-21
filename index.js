a5ca5b3d066571fb51215642664a164313a1dfee
kollosp <pparczyk@wp.pl>
2018-12-21 23:40:14 +0100

    file system, materials from kuba, needed packages, backend files ffor files serving and basic structure of main page

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+
+//express settings
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/web/frontend'))
+app.use('/lib', express.static(__dirname + '/lib'))
+
+//routers include
+const index = require('./web/backend/index')
+
+
+//routing array
+app.use('/', index)
+
+
+app.listen(3000, () => {
+	console.log('server is listening on port 3000')
+})
